f9cefe37a41e3a9fe8ec5418780431ff4c81d47b
Peter Hang <peterhanggg@gmail.com>
2019-12-16 17:28:37 -0500

    created a context component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Context from "./components/Context/Context";
 
 function App() {
   return (
     <div className="App">
-    
+      <Context />
     </div>
   );
 }
---
+++ src/components/Context/Context.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Context.scss";
+
+export default function Context() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
