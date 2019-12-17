4550afe1b70a3840ded51e384327a3da62b28a89
Peter Hang <peterhanggg@gmail.com>
2019-12-16 20:14:00 -0500

    added projects component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
