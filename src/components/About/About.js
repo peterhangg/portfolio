b636b4891053bca92670e89ba149782cc31b4e7f
Peter Hang <peterhanggg@gmail.com>
2019-12-19 17:43:43 -0500

    added animate css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./About.scss";
+import "../../animate.scss";
 
 const frontend = ["react", "javascript", "sass", "css", "jquery", "bootstrap", "html"];
 const backend = ["node", "expressjs", "rails", "postgreSQL", "mySQL"];
@@ -8,7 +9,7 @@ const currentLearning = ["redux", "mongoDB", "graphQL"];
 export default function About(props) {
   return (
     <div className="about-section" id="About">
-      <h1 className="about-headings">ABOUT</h1>
+      <h1 className="about-headings animated slideInLeft">ABOUT</h1>
       <div className="about-info">
         <img src="https://picsum.photos/id/237/300/300" alt="Peter Hang"/>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, veritatis aliquam consequuntur fuga unde error odit nulla illum debitis porro dolorem. Quam odio provident, quae nam asperiores aperiam iste dolorum?</p>
@@ -18,19 +19,19 @@ export default function About(props) {
         <div className="tech-stack">
           <h2>Frontend</h2>
           {frontend.map((img, index) => (
-            <img key={index} className="stack-img" src={`../icons/${img}.png`} alt={`${img}`}/>
+            <img key={index} className="stack-img animated fadeInRight" src={`../icons/${img}.png`} alt={`${img}`}/>
           ))} 
         </div>
         <div>
           <h2>Backend</h2>
           {backend.map((img, index) => (
-            <img key={index} className="stack-img" src={`../icons/${img}.png`} alt={`${img}`}/>
+            <img key={index} className="stack-img animated fadeInRight" src={`../icons/${img}.png`} alt={`${img}`}/>
           ))} 
         </div>
         <div>
           <h2>Current Learning</h2>
           {currentLearning.map((img, index) => (
-            <img key={index} className="stack-img" src={`../icons/${img}.png`} alt={`${img}`}/>
+            <img key={index} className="stack-img animated fadeInRight" src={`../icons/${img}.png`} alt={`${img}`}/>
           ))} 
         </div>
       </div>
