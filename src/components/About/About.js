c4bba6e25d2293d26d72cd2e80ffaaa69cf440b7
Peter Hang <peterhanggg@gmail.com>
2019-12-19 15:43:30 -0500

    added stack images

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./About.scss";
+const frontend = ["react", "javascript", "sass", "css", "jquery", "bootstrap", "html"];
 
-export default function About() {
+export default function About(props) {
   return (
     <div className="about-section" id="About">
       <h1>ABOUT</h1>
@@ -10,7 +11,12 @@ export default function About() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, veritatis aliquam consequuntur fuga unde error odit nulla illum debitis porro dolorem. Quam odio provident, quae nam asperiores aperiam iste dolorum?</p>
       </div>
       <div className="skills-section">
-        
+        <div className="front-end">
+          <h2>Front-end</h2>
+          {frontend.map((img, index) => (
+            <img key={index}src={`../icons/${img}.png`} alt={`${img}`}/>
+          ))} 
+        </div>
       </div>
     </div>
   )
