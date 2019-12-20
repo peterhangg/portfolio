fc20aaeb0c2c2b2ac9161366ebb09752da7557f1
Peter Hang <peterhanggg@gmail.com>
2019-12-20 15:47:24 -0500

    edited about section to display in flex row

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,37 +2,39 @@ import React from 'react'
 import "./About.scss";
 import "../../animate.scss";
 
-const frontend = ["react", "javascript", "sass", "css", "jquery", "bootstrap", "html"];
+const frontend = ["react", "javascript", "sass", "css", "jQuery", "bootstrap", "html"];
 const backend = ["node", "expressjs", "rails", "postgreSQL", "mySQL"];
 const currentLearning = ["redux", "mongoDB", "graphQL"];
 
 export default function About(props) {
   return (
-    <div className="about-section" id="About">
-      <h1 className="about-headings animated slideInLeft">ABOUT</h1>
-      <div className="about-info">
-        <img src="https://picsum.photos/id/237/300/300" alt="Peter Hang"/>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, veritatis aliquam consequuntur fuga unde error odit nulla illum debitis porro dolorem. Quam odio provident, quae nam asperiores aperiam iste dolorum?</p>
-      </div>
-      <div className="skills-section">
-        <h1>Tech Stack</h1>
-        <div className="tech-stack">
-          <h2>Frontend</h2>
-          {frontend.map((img, index) => (
-            <img key={index} className="stack-img animated fadeInRight" src={`../icons/${img}.png`} alt={`${img}`}/>
-          ))} 
+    <div  id="About">
+      <h1 className="about-heading animated slideInLeft">ABOUT</h1>
+      <p className="underline animated fadeInLeft delay-1s"></p>
+      <div className="about-section">
+        <div className="about-info animated fadeInLeft">
+          <img src="https://picsum.photos/id/237/300/300" alt="Peter Hang"/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, veritatis aliquam consequuntur fuga unde error odit nulla illum debitis porro dolorem. Quam odio provident, quae nam asperiores aperiam iste dolorum?</p>
         </div>
-        <div>
-          <h2>Backend</h2>
-          {backend.map((img, index) => (
-            <img key={index} className="stack-img animated fadeInRight" src={`../icons/${img}.png`} alt={`${img}`}/>
-          ))} 
-        </div>
-        <div>
-          <h2>Current Learning</h2>
-          {currentLearning.map((img, index) => (
-            <img key={index} className="stack-img animated fadeInRight" src={`../icons/${img}.png`} alt={`${img}`}/>
-          ))} 
+        <div className="skills-section animated fadeInRight">
+          <div className="tech-stack">
+            <h2>Frontend</h2>
+            {frontend.map((img, index) => (
+              <img key={index} className="stack-img" src={`../icons/${img}.png`} alt={`${img}`}/>
+            ))} 
+          </div>
+          <div>
+            <h2>Backend</h2>
+            {backend.map((img, index) => (
+              <img key={index} className="stack-img" src={`../icons/${img}.png`} alt={`${img}`}/>
+            ))} 
+          </div>
+          <div>
+            <h2>Current Learning</h2>
+            {currentLearning.map((img, index) => (
+              <img key={index} className="stack-img" src={`../icons/${img}.png`} alt={`${img}`}/>
+            ))} 
+          </div>
         </div>
       </div>
     </div>
