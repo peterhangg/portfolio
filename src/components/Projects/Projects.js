c8306ffd1aecd10a0777aa15a7e543c7f1a1f514
Peter Hang <peterhanggg@gmail.com>
2019-12-20 17:03:13 -0500

    added PorjectCards component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Projects.scss";
+import ProjectCards from "./ProjectCards";
 
 export default function Projects() {
   return (
     <div>
       <h1>Hello from Projects</h1>
+      <ProjectCards />
     </div>
   )
 }
